dcb1e3dfefa8e34013c22728421b409ee48b7ee1
David Heinemeier Hansson <david@loudthinking.com>
2011-11-27 16:03:55 +0100

    Superclass mismatch

--- lib/railtie.rb
+++ lib/railtie.rb
@@ -1,4 +1,4 @@
-class Jbuilder
+class Jbuilder < BlankSlate
   class Railtie < Rails::Railtie
     initializer "jbuilder.template_handler" do
       ActionView::Template.register_template_handler :jbuilder, -> template { "Jbuilder.encode do |json|;#{template.source};end;" }
