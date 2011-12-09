25177cf8ae8448d43f4c3a2756a47f3b19609621
Andrew White <andyw@pixeltrix.co.uk>
2011-12-09 10:04:18 +0000

    Use args.size in preference to one? and many?
    The Enumerable method one? behaves in a similar manner to the any?
    method such that it checks if the enum has one element that is
    truthy. This obviously causes problems when trying to set false/nil
    values as args.one? will return false.
    
    Although many? does behave differently to one? and any? in that it
    depends on the actual number of elements in the enum its use of a
    block argument causes a performance impact on Ruby 1.8.7.

--- test/jbuilder_test.rb
+++ test/jbuilder_test.rb
@@ -8,7 +8,7 @@ class JbuilderTest < ActiveSupport::TestCase
     json = Jbuilder.encode do |json|
       json.content "hello"
     end
-    
+
     assert_equal "hello", JSON.parse(json)["content"]
   end
 
@@ -17,39 +17,39 @@ class JbuilderTest < ActiveSupport::TestCase
       json.title "hello"
       json.content "world"
     end
-    
+
     JSON.parse(json).tap do |parsed|
       assert_equal "hello", parsed["title"]
       assert_equal "world", parsed["content"]
     end
   end
-  
+
   test "extracting from object" do
     person = Struct.new(:name, :age).new("David", 32)
-    
+
     json = Jbuilder.encode do |json|
       json.extract! person, :name, :age
     end
-    
+
     JSON.parse(json).tap do |parsed|
       assert_equal "David", parsed["name"]
       assert_equal 32, parsed["age"]
     end
   end
-  
+
   test "extracting from object using call style for 1.9" do
     person = Struct.new(:name, :age).new("David", 32)
-    
+
     json = Jbuilder.encode do |json|
       json.(person, :name, :age)
     end
-    
+
     JSON.parse(json).tap do |parsed|
       assert_equal "David", parsed["name"]
       assert_equal 32, parsed["age"]
     end
   end
-  
+
   test "nesting single child with block" do
     json = Jbuilder.encode do |json|
       json.author do |json|
@@ -57,13 +57,13 @@ class JbuilderTest < ActiveSupport::TestCase
         json.age  32
       end
     end
-    
+
     JSON.parse(json).tap do |parsed|
       assert_equal "David", parsed["author"]["name"]
       assert_equal 32, parsed["author"]["age"]
     end
   end
-  
+
   test "nesting multiple children with block" do
     json = Jbuilder.encode do |json|
       json.comments do |json|
@@ -77,50 +77,50 @@ class JbuilderTest < ActiveSupport::TestCase
       assert_equal "world", parsed["comments"].second["content"]
     end
   end
-  
+
   test "nesting single child with inline extract" do
     person = Class.new do
       attr_reader :name, :age
-      
+
       def initialize(name, age)
         @name, @age = name, age
       end
     end.new("David", 32)
-    
+
     json = Jbuilder.encode do |json|
       json.author person, :name, :age
     end
-    
+
     JSON.parse(json).tap do |parsed|
       assert_equal "David", parsed["author"]["name"]
       assert_equal 32,      parsed["author"]["age"]
     end
   end
-  
+
   test "nesting multiple children from array" do
     comments = [ Struct.new(:content, :id).new("hello", 1), Struct.new(:content, :id).new("world", 2) ]
-    
+
     json = Jbuilder.encode do |json|
       json.comments comments, :content
     end
-    
+
     JSON.parse(json).tap do |parsed|
       assert_equal ["content"], parsed["comments"].first.keys
       assert_equal "hello", parsed["comments"].first["content"]
       assert_equal "world", parsed["comments"].second["content"]
     end
   end
-  
-  
+
+
   test "nesting multiple children from array with inline loop" do
     comments = [ Struct.new(:content, :id).new("hello", 1), Struct.new(:content, :id).new("world", 2) ]
-    
+
     json = Jbuilder.encode do |json|
       json.comments comments do |json, comment|
         json.content comment.content
       end
     end
-    
+
     JSON.parse(json).tap do |parsed|
       assert_equal ["content"], parsed["comments"].first.keys
       assert_equal "hello", parsed["comments"].first["content"]
@@ -130,42 +130,42 @@ class JbuilderTest < ActiveSupport::TestCase
 
   test "nesting multiple children from array with inline loop on root" do
     comments = [ Struct.new(:content, :id).new("hello", 1), Struct.new(:content, :id).new("world", 2) ]
-    
+
     json = Jbuilder.encode do |json|
       json.(comments) do |json, comment|
         json.content comment.content
       end
     end
-    
+
     JSON.parse(json).tap do |parsed|
       assert_equal "hello", parsed.first["content"]
       assert_equal "world", parsed.second["content"]
     end
   end
-  
+
   test "array nested inside nested hash" do
     json = Jbuilder.encode do |json|
       json.author do |json|
         json.name "David"
         json.age  32
-        
+
         json.comments do |json|
           json.child! { |json| json.content "hello" }
           json.child! { |json| json.content "world" }
         end
       end
     end
-    
+
     JSON.parse(json).tap do |parsed|
       assert_equal "hello", parsed["author"]["comments"].first["content"]
       assert_equal "world", parsed["author"]["comments"].second["content"]
     end
   end
-  
+
   test "array nested inside array" do
     json = Jbuilder.encode do |json|
       json.comments do |json|
-        json.child! do |json| 
+        json.child! do |json|
           json.authors do |json|
             json.child! do |json|
               json.name "david"
@@ -174,10 +174,10 @@ class JbuilderTest < ActiveSupport::TestCase
         end
       end
     end
-    
+
     assert_equal "david", JSON.parse(json)["comments"].first["authors"].first["name"]
   end
-  
+
   test "top-level array" do
     comments = [ Struct.new(:content, :id).new("hello", 1), Struct.new(:content, :id).new("world", 2) ]
 
@@ -186,18 +186,33 @@ class JbuilderTest < ActiveSupport::TestCase
         json.content comment.content
       end
     end
-    
+
     JSON.parse(json).tap do |parsed|
       assert_equal "hello", parsed.first["content"]
       assert_equal "world", parsed.second["content"]
     end
-  end 
-  
+  end
+
   test "dynamically set a key/value" do
     json = Jbuilder.encode do |json|
       json.set!(:each, "stuff")
     end
-    
+
     assert_equal "stuff", JSON.parse(json)["each"]
+  end
+
+  test "false/nil values can be set" do
+    json = Jbuilder.encode do |json|
+      json.false_value false
+      json.nil_value nil
+    end
+
+    JSON.parse(json).tap do |parsed|
+      assert parsed.key?("false_value"), "The false value key is not present"
+      assert_equal false, parsed["false_value"]
+
+      assert parsed.key?("nil_value"), "The nil value key is not present"
+      assert_equal nil, parsed["nil_value"]
+    end
   end
 end
