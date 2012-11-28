2c820f6c1ec370fcf129ce6b8b5b0dbbd70ab848
David Heinemeier Hansson <david@loudthinking.com>
2012-11-28 18:32:51 +0100

    Revert "Revert ".each.map is redundant" -- see "query like object" test." -- on second thought, it's reasonable to expect a full Enumerable.
    This reverts commit 6dd70ba3f9f3e5f8336a9d03d946af27a834e191.

--- lib/jbuilder.rb
+++ lib/jbuilder.rb
@@ -291,7 +291,7 @@ class Jbuilder < ActiveSupport::BasicObject
     end
 
     def _map_collection(collection)
-      collection.each.map do |element|
+      collection.map do |element|
         _scope { yield element }
       end
     end
