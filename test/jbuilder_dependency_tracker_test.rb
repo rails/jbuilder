8e73cea39f60da1384afd687cc8e5e399630d8cc
Pavel Pravosud <pavel@pravosud.com>
2013-10-12 18:29:48 -0400

    Track template dependencies
    closes #126

---
+++ test/jbuilder_dependency_tracker_test.rb
@@ -0,0 +1,63 @@
+require 'test/unit'
+require 'active_support/test_case'
+require 'jbuilder/dependency_tracker'
+
+
+class FakeTemplate
+    attr_reader :source, :handler
+    def initialize(source, handler = :jbuilder)
+      @source, @handler = source, handler
+    end
+end
+
+
+class JbuilderDependencyTrackerTest < ActiveSupport::TestCase
+    def make_tracker(name, source)
+      template = FakeTemplate.new(source)
+      Jbuilder::DependencyTracker.new(name, template)
+    end
+
+    def track_dependencies(source)
+      make_tracker('jbuilder_template', source).dependencies
+    end
+
+    test 'detects dependency via direct partial! call' do
+      dependencies = track_dependencies <<-RUBY
+        json.partial! 'path/to/partial'
+      RUBY
+
+      assert_equal %w[path/to/partial], dependencies
+    end
+
+    test 'detects dependency via direct partial! call with parens' do
+      dependencies = track_dependencies <<-RUBY
+        json.partial!("path/to/partial")
+      RUBY
+
+      assert_equal %w[path/to/partial], dependencies
+    end
+
+    test 'detects partial with options (1.9 style)' do
+      dependencies = track_dependencies <<-RUBY
+        json.partial! hello: 'world', partial: 'path/to/partial', foo: :bar
+      RUBY
+
+      assert_equal %w[path/to/partial], dependencies
+    end
+
+    test 'detects partial with options (1.8 style)' do
+      dependencies = track_dependencies <<-RUBY
+        json.partial! :hello => 'world', :partial => 'path/to/partial', :foo => :bar
+      RUBY
+
+      assert_equal %w[path/to/partial], dependencies
+    end
+
+    test 'detects partial in indirect collecton calls' do
+      dependencies = track_dependencies <<-RUBY
+        json.comments @post.comments, partial: 'comments/comment', as: :comment
+      RUBY
+
+      assert_equal %w[comments/comment], dependencies
+    end
+end
