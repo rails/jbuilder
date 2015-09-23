45c6cadc4bb163ec8ebab29541e765795c08a206
Al Snow <jasnow@hotmail.com>
2015-09-23 12:03:35 -0400

    Tried :JSON inside []s

--- lib/jbuilder/jbuilder_template.rb
+++ lib/jbuilder/jbuilder_template.rb
@@ -170,7 +170,7 @@ end
 
 class JbuilderHandler
   cattr_accessor :default_format
-  self.default_format = Mime::Type:JSON
+  self.default_format = Mime::Type[:JSON]
 
   def self.call(template)
     # this juggling is required to keep line numbers right in the error
