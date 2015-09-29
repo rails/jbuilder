5ba4e4ac654cc8388619538f576fe234659b84ec
Jeremy Daer <jeremydaer@gmail.com>
2015-09-29 14:26:25 -0700

    Shush Mime::JSON deprecation. Switch to backward-compatible Mime[:json]

--- lib/jbuilder/jbuilder_template.rb
+++ lib/jbuilder/jbuilder_template.rb
@@ -170,7 +170,7 @@ end
 
 class JbuilderHandler
   cattr_accessor :default_format
-  self.default_format = Mime::JSON
+  self.default_format = Mime[:json]
 
   def self.call(template)
     # this juggling is required to keep line numbers right in the error
