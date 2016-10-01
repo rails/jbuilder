5ed9f0163a5205c04d7abbbd4ddfc3a3bbe4d386
thinca <thinca@gmail.com>
2016-10-01 18:16:07 +0900

    Avoid warning on test
    > warning: ambiguous first argument; put parentheses or a space even after `/' operator

--- test/scaffold_api_controller_generator_test.rb
+++ test/scaffold_api_controller_generator_test.rb
@@ -15,7 +15,7 @@ if Rails::VERSION::MAJOR > 4
 
       assert_file 'app/controllers/posts_controller.rb' do |content|
         assert_instance_method :index, content do |m|
-          assert_match /@posts = Post\.all/, m
+          assert_match(/@posts = Post\.all/, m)
         end
 
         assert_instance_method :show, content do |m|
@@ -23,19 +23,19 @@ if Rails::VERSION::MAJOR > 4
         end
 
         assert_instance_method :create, content do |m|
-          assert_match /@post = Post\.new\(post_params\)/, m
-          assert_match /@post\.save/, m
-          assert_match /render :show, status: :created, location: @post/, m
-          assert_match /render json: @post\.errors, status: :unprocessable_entity/, m
+          assert_match(/@post = Post\.new\(post_params\)/, m)
+          assert_match(/@post\.save/, m)
+          assert_match(/render :show, status: :created, location: @post/, m)
+          assert_match(/render json: @post\.errors, status: :unprocessable_entity/, m)
         end
 
         assert_instance_method :update, content do |m|
-          assert_match /render :show, status: :ok, location: @post/, m
-          assert_match /render json: @post.errors, status: :unprocessable_entity/, m
+          assert_match(/render :show, status: :ok, location: @post/, m)
+          assert_match(/render json: @post.errors, status: :unprocessable_entity/, m)
         end
 
         assert_instance_method :destroy, content do |m|
-          assert_match /@post\.destroy/, m
+          assert_match(/@post\.destroy/, m)
         end
 
         assert_match(/def post_params/, content)
