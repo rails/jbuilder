2fc446f0aab63beaca5806fa9c838f4586ec4630
thinca <thinca@gmail.com>
2016-10-02 17:21:50 +0900

    Avoid warning on test (#360)
    > warning: ambiguous first argument; put parentheses or a space even after `/' operator

--- test/scaffold_api_controller_generator_test.rb
+++ test/scaffold_api_controller_generator_test.rb
@@ -15,7 +15,7 @@ if Rails::VERSION::MAJOR > 4
 
       assert_file 'app/controllers/posts_controller.rb' do |content|
         assert_instance_method :index, content do |m|
-          assert_match /@posts = Post\.all/, m
+          assert_match %r{@posts = Post\.all}, m
         end
 
         assert_instance_method :show, content do |m|
@@ -23,23 +23,23 @@ if Rails::VERSION::MAJOR > 4
         end
 
         assert_instance_method :create, content do |m|
-          assert_match /@post = Post\.new\(post_params\)/, m
-          assert_match /@post\.save/, m
-          assert_match /render :show, status: :created, location: @post/, m
-          assert_match /render json: @post\.errors, status: :unprocessable_entity/, m
+          assert_match %r{@post = Post\.new\(post_params\)}, m
+          assert_match %r{@post\.save}, m
+          assert_match %r{render :show, status: :created, location: @post}, m
+          assert_match %r{render json: @post\.errors, status: :unprocessable_entity}, m
         end
 
         assert_instance_method :update, content do |m|
-          assert_match /render :show, status: :ok, location: @post/, m
-          assert_match /render json: @post.errors, status: :unprocessable_entity/, m
+          assert_match %r{render :show, status: :ok, location: @post}, m
+          assert_match %r{render json: @post.errors, status: :unprocessable_entity}, m
         end
 
         assert_instance_method :destroy, content do |m|
-          assert_match /@post\.destroy/, m
+          assert_match %r{@post\.destroy}, m
         end
 
-        assert_match /def post_params/, content
-        assert_match /params\.require\(:post\)\.permit\(:title, :body\)/, content
+        assert_match %r{def post_params}, content
+        assert_match %r{params\.require\(:post\)\.permit\(:title, :body\)}, content
       end
     end
 
@@ -47,8 +47,8 @@ if Rails::VERSION::MAJOR > 4
       run_generator %w(Post --api)
 
       assert_file 'app/controllers/posts_controller.rb' do |content|
-        assert_match /def post_params/, content
-        assert_match /params\.fetch\(:post, {}\)/, content
+        assert_match %r{def post_params}, content
+        assert_match %r{params\.fetch\(:post, \{\}\)}, content
       end
     end
   end
--- test/scaffold_controller_generator_test.rb
+++ test/scaffold_controller_generator_test.rb
@@ -13,7 +13,7 @@ class ScaffoldControllerGeneratorTest < Rails::Generators::TestCase
 
     assert_file 'app/controllers/posts_controller.rb' do |content|
       assert_instance_method :index, content do |m|
-        assert_match /@posts = Post\.all/, m
+        assert_match %r{@posts = Post\.all}, m
       end
 
       assert_instance_method :show, content do |m|
@@ -21,7 +21,7 @@ class ScaffoldControllerGeneratorTest < Rails::Generators::TestCase
       end
 
       assert_instance_method :new, content do |m|
-        assert_match /@post = Post\.new/, m
+        assert_match %r{@post = Post\.new}, m
       end
 
       assert_instance_method :edit, content do |m|
@@ -29,29 +29,29 @@ class ScaffoldControllerGeneratorTest < Rails::Generators::TestCase
       end
 
       assert_instance_method :create, content do |m|
-        assert_match /@post = Post\.new\(post_params\)/, m
-        assert_match /@post\.save/, m
-        assert_match /format\.html \{ redirect_to @post, notice: 'Post was successfully created\.' \}/, m
-        assert_match /format\.json \{ render :show, status: :created, location: @post \}/, m
-        assert_match /format\.html \{ render :new \}/, m
-        assert_match /format\.json \{ render json: @post\.errors, status: :unprocessable_entity \}/, m
+        assert_match %r{@post = Post\.new\(post_params\)}, m
+        assert_match %r{@post\.save}, m
+        assert_match %r{format\.html \{ redirect_to @post, notice: 'Post was successfully created\.' \}}, m
+        assert_match %r{format\.json \{ render :show, status: :created, location: @post \}}, m
+        assert_match %r{format\.html \{ render :new \}}, m
+        assert_match %r{format\.json \{ render json: @post\.errors, status: :unprocessable_entity \}}, m
       end
 
       assert_instance_method :update, content do |m|
-        assert_match /format\.html \{ redirect_to @post, notice: 'Post was successfully updated\.' \}/, m
-        assert_match /format\.json \{ render :show, status: :ok, location: @post \}/, m
-        assert_match /format\.html \{ render :edit \}/, m
-        assert_match /format\.json \{ render json: @post.errors, status: :unprocessable_entity \}/, m
+        assert_match %r{format\.html \{ redirect_to @post, notice: 'Post was successfully updated\.' \}}, m
+        assert_match %r{format\.json \{ render :show, status: :ok, location: @post \}}, m
+        assert_match %r{format\.html \{ render :edit \}}, m
+        assert_match %r{format\.json \{ render json: @post.errors, status: :unprocessable_entity \}}, m
       end
 
       assert_instance_method :destroy, content do |m|
-        assert_match /@post\.destroy/, m
-        assert_match /format\.html \{ redirect_to posts_url, notice: 'Post was successfully destroyed\.' \}/, m
-        assert_match /format\.json \{ head :no_content \}/, m
+        assert_match %r{@post\.destroy}, m
+        assert_match %r{format\.html \{ redirect_to posts_url, notice: 'Post was successfully destroyed\.' \}}, m
+        assert_match %r{format\.json \{ head :no_content \}}, m
       end
 
-      assert_match /def post_params/, content
-      assert_match /params\.require\(:post\)\.permit\(:title, :body\)/, content
+      assert_match %r{def post_params}, content
+      assert_match %r{params\.require\(:post\)\.permit\(:title, :body\)}, content
     end
   end
 
@@ -59,8 +59,8 @@ class ScaffoldControllerGeneratorTest < Rails::Generators::TestCase
     run_generator %w(Post)
 
     assert_file 'app/controllers/posts_controller.rb' do |content|
-      assert_match /def post_params/, content
-      assert_match /params\.fetch\(:post, {}\)/, content
+      assert_match %r{def post_params}, content
+      assert_match %r{params\.fetch\(:post, \{\}\)}, content
     end
   end
 end
