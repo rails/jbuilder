f4350103bc3c93163659b287226171266511f27c
PikachuEXE <pikachuexe@gmail.com>
2017-11-28 09:35:30 +0800

    * Test for multiple properties inside capture and inject test

--- test/turbo_streamer/capture_test.rb
+++ test/turbo_streamer/capture_test.rb
@@ -14,6 +14,7 @@ class TurboStreamer::CaptureTest < ActiveSupport::TestCase
         builder.key1 'value1'
         capture = builder.__send__(:_capture) do
           builder.key2 'value2'
+          builder.key4 'value4'
         end
         builder.key3 'value3'
       end
@@ -21,7 +22,7 @@ class TurboStreamer::CaptureTest < ActiveSupport::TestCase
       Wankel::DEFAULTS[:write_buffer_size] = old_buf_size
     end
 
-    assert_equal '"key2":"value2"', capture
+    assert_equal '"key2":"value2","key4":"value4"', capture
     assert_equal '{"key1":"value1","key3":"value3"}', builder.target!
   end
 
--- test/turbo_streamer/inject_test.rb
+++ test/turbo_streamer/inject_test.rb
@@ -24,4 +24,23 @@ class TurboStreamer::InjectTest < ActiveSupport::TestCase
     assert_equal 'Pavel', result['author']['name']
   end
 
+  test 'support inject! method in a block with a string with multiple keys' do
+    result = jbuild do |json|
+      json.object! do
+        json.author do
+          json.object! do
+            json.attr1 "value1"
+            json.inject! '"name":"Pavel","age":30'
+            json.attr2 "value2"
+          end
+        end
+      end
+    end
+
+    assert_equal 'Pavel', result['author']['name']
+    assert_equal 30, result['author']['age']
+    assert_equal 'value1', result['author']['attr1']
+    assert_equal 'value2', result['author']['attr2']
+  end
+
 end
