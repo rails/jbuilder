6c499b5a884fdeafef28bc64155546b167ae11f5
Jon Bracy <jonbracy@gmail.com>
2017-11-28 23:00:08 -0500

    Clean up CaptureTest

--- test/turbo_streamer/capture_test.rb
+++ test/turbo_streamer/capture_test.rb
@@ -3,23 +3,16 @@ require 'test_helper'
 class TurboStreamer::CaptureTest < ActiveSupport::TestCase
 
   test "_capture" do
-    old_buf_size = Wankel::DEFAULTS[:write_buffer_size]
     builder = TurboStreamer.new
     capture = nil
 
-    begin
-      Wankel::DEFAULTS[:write_buffer_size]  = 1_000_000
-
-      builder.object! do
-        builder.key1 'value1'
-        capture = builder.__send__(:_capture) do
-          builder.key2 'value2'
-          builder.key4 'value4'
-        end
-        builder.key3 'value3'
+    builder.object! do
+      builder.key1 'value1'
+      capture = builder.__send__(:_capture) do
+        builder.key2 'value2'
+        builder.key4 'value4'
       end
-    ensure
-      Wankel::DEFAULTS[:write_buffer_size] = old_buf_size
+      builder.key3 'value3'
     end
 
     # Strip here because OJ adds new lines for unkown reasons
