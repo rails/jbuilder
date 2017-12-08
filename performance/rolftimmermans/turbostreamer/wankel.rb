7f215c405d1ebe75d59e00eece462ec396135f1b
Jon Bracy <jonbracy@gmail.com>
2017-12-08 15:27:40 -0800

    Fix rolftimmermans test

--- performance/rolftimmermans/turbostreamer/wankel.rb
+++ performance/rolftimmermans/turbostreamer/wankel.rb
@@ -4,28 +4,30 @@ TurboStreamer.set_default_encoder(:json, :wankel)
 __SETUP__
 
 TurboStreamer.encode(encoder: :wankel) do |json|
-  json.article do
-    json.object! do
-      json.author do
-        json.object! do
-          json.extract!($author, :name, :birthyear, :bio)
+  json.object! do
+    json.article do
+      json.object! do
+        json.author do
+          json.object! do
+            json.extract!($author, :name, :birthyear, :bio)
+          end
         end
-      end
-      json.title "Profiling Jbuilder"
-      json.body "How to profile Jbuilder"
-      json.date $now
-      json.references $arr do |ref|
-        json.object! do
-          json.name "Introduction to profiling"
-          json.url "http://example.com/"
+        json.title "Profiling Jbuilder"
+        json.body "How to profile Jbuilder"
+        json.date $now
+        json.references $arr do |ref|
+          json.object! do
+            json.name "Introduction to profiling"
+            json.url "http://example.com/"
+          end
         end
-      end
-      json.comments $arr do |comment|
-        json.object! do
-          json.author($author, :name, :birthyear, :bio)
-          json.email "rolf@example.com"
-          json.body "Great article"
-          json.date $now
+        json.comments $arr do |comment|
+          json.object! do
+            json.author($author, :name, :birthyear, :bio)
+            json.email "rolf@example.com"
+            json.body "Great article"
+            json.date $now
+          end
         end
       end
     end
