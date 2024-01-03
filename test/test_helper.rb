9db282b5aef2e79134ecfa6340984547e365e681
Akira Matsuda <ronnie@dio.jp>
2024-01-04 03:25:20 +0900

    Run AC & AV railties on the testing app

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,6 +8,8 @@ require "active_support/core_ext/array/access"
 require "active_support/cache/memory_store"
 require "active_support/json"
 require "active_model"
+require 'action_controller/railtie'
+require 'action_view/railtie'
 
 require "active_support/testing/autorun"
 require "mocha/minitest"
