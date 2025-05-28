a4dc35ef73b655e0af6f97bef2bdd61b0cde5d37
Michael Oberegger <michael.oberegger@affinity.co>
2025-05-28 16:34:08 -0400

    Cleanup Railtie for Rails 7

--- lib/jbuilder/railtie.rb
+++ lib/jbuilder/railtie.rb
@@ -9,28 +9,24 @@ class Jbuilder
         require 'jbuilder/jbuilder_dependency_tracker'
       end
 
-      if Rails::VERSION::MAJOR >= 5
-        module ::ActionController
-          module ApiRendering
-            include ActionView::Rendering
-          end
+      module ::ActionController
+        module ApiRendering
+          include ActionView::Rendering
         end
+      end
 
-        ActiveSupport.on_load :action_controller do
-          if name == 'ActionController::API'
-            include ActionController::Helpers
-            include ActionController::ImplicitRender
-          end
+      ActiveSupport.on_load :action_controller do
+        if name == 'ActionController::API'
+          include ActionController::Helpers
+          include ActionController::ImplicitRender
         end
       end
     end
 
-    if Rails::VERSION::MAJOR >= 4
-      generators do |app|
-        Rails::Generators.configure! app.config.generators
-        Rails::Generators.hidden_namespaces.uniq!
-        require 'generators/rails/scaffold_controller_generator'
-      end
+    generators do |app|
+      Rails::Generators.configure! app.config.generators
+      Rails::Generators.hidden_namespaces.uniq!
+      require 'generators/rails/scaffold_controller_generator'
     end
   end
 end
