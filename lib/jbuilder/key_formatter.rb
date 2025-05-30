7c58bcc5dcddec8695e938a35e4c4c581c08f537
Michael Oberegger <michael.oberegger@affinity.co>
2025-05-30 10:11:05 -0400

    Optimize key formatter

--- lib/jbuilder/key_formatter.rb
+++ lib/jbuilder/key_formatter.rb
@@ -1,34 +1,26 @@
 require 'jbuilder/jbuilder'
-require 'active_support/core_ext/array'
 
 class Jbuilder
   class KeyFormatter
-    def initialize(*args)
-      @format = {}
-      @cache = {}
+    def initialize(*formats, **formats_with_options)
+      @cache =
+        Hash.new do |hash, key|
+          value = key.is_a?(Symbol) ? key.name : key.to_s
 
-      options = args.extract_options!
-      args.each do |name|
-        @format[name] = []
-      end
-      options.each do |name, parameters|
-        @format[name] = parameters
-      end
-    end
+          formats.each do |func|
+            value = func.is_a?(Proc) ? func.call(value) : value.send(func)
+          end
 
-    def initialize_copy(original)
-      @cache = {}
+          formats_with_options.each do |func, params|
+            value = func.is_a?(Proc) ? func.call(value, *params) : value.send(func, *params)
+          end
+
+          hash[key] = value
+        end
     end
 
     def format(key)
-      @cache[key] ||= @format.inject(key.to_s) do |result, args|
-        func, args = args
-        if ::Proc === func
-          func.call result, *args
-        else
-          result.send func, *args
-        end
-      end
+      @cache[key]
     end
   end
 end
