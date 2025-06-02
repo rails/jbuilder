a6f1eedc435040e94b11230ab405a3b8ee1f5f1f
Michael Oberegger <michael.oberegger@affinity.co>
2025-06-02 16:05:30 -0400

    Use mutex for thread safety

--- lib/jbuilder/key_formatter.rb
+++ lib/jbuilder/key_formatter.rb
@@ -3,24 +3,28 @@ require 'jbuilder/jbuilder'
 class Jbuilder
   class KeyFormatter
     def initialize(*formats, **formats_with_options)
-      @cache =
-        Hash.new do |hash, key|
-          value = key.is_a?(Symbol) ? key.name : key.to_s
-
-          formats.each do |func|
-            value = func.is_a?(Proc) ? func.call(value) : value.send(func)
-          end
-
-          formats_with_options.each do |func, params|
-            value = func.is_a?(Proc) ? func.call(value, *params) : value.send(func, *params)
-          end
-
-          hash[key] = value
-        end
+      @mutex = Mutex.new
+      @formats = formats
+      @formats_with_options = formats_with_options
+      @cache = {}
     end
 
     def format(key)
-      @cache[key]
+      @mutex.synchronize do
+        @cache[key] ||= begin
+          value = key.is_a?(Symbol) ? key.name : key.to_s
+
+          @formats.each do |func|
+            value = func.is_a?(Proc) ? func.call(value) : value.send(func)
+          end
+
+          @formats_with_options.each do |func, params|
+            value = func.is_a?(Proc) ? func.call(value, *params) : value.send(func, *params)
+          end
+
+          value
+        end
+      end
     end
   end
 end
